72f3d5220b3814b3267ea779f3a1cfe25bab2b43
youssef lab20-u <yousseflab20@gmail.com>
2025-12-04 00:16:19 +0100

    host your find version for HomeScreen

--- mobile/index.js
+++ mobile/index.js
@@ -3,7 +3,7 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from './app/screen/LoginUI';
+import App from './app/screen/HomeScren';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
