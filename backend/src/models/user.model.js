74a1e8a8f130d961f7a83d6ce6c6385545ac7f2f
youssef lab20-u <yousseflab20@gmail.com>
2025-12-07 14:15:19 +0100

    add your User for DataType for Table

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,41 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/DataBase.js";
+import bcrypt from "bcrypt";
+
+export const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "users",
+    timestamps: false,
+    hooks: {
+      beforeCreate: async (user) => {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      },
+    },
+  }
+);
